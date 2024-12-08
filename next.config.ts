0a5b6cd42326bb960dd291f095146b0b69b66f59
sbin0819 <sbinha123@gmail.com>
2024-12-08 12:55:29 +0900

    chore: some changed

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  async rewrites() {
-    return [
-      {
-        source: '/:path*',
-        destination: 'https://openapi.naver.com/:path*',
-      },
-    ];
-  },
 };
 
 export default nextConfig;
