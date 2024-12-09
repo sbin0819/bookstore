e1b37b6d1c22092501d63764ba8a1e5d42ca0354
sbin0819 <sbinha123@gmail.com>
2024-12-09 11:33:04 +0900

    chore: api 수정

--- app/api/recommendation/route.ts
+++ app/api/recommendation/route.ts
@@ -1,7 +1,5 @@
 import { openApiInstance } from '@/services/base';
 
-export const dynamic = 'force-static';
-
 export async function GET(): Promise<Response> {
   const data = await openApiInstance.get(
     '/book.json?query=자바스크립트&display=30'
