5f1a81135a6d06dd3b8e3cf16aae04e28046589e
sbin0819 <sbinha123@gmail.com>
2024-12-10 10:06:33 +0900

    fix: build error

--- services/base.ts
+++ services/base.ts
@@ -1,10 +1,7 @@
 import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
 
 export const OPEN_API_URL = 'https://openapi.naver.com/v1/search';
-export const BASE_API_URL =
-  process.env.NEXT_PUBLIC_ENV === 'production'
-    ? 'https://bookstore-server-sigma.vercel.app/'
-    : 'http://localhost:4000';
+export const BASE_API_URL = 'https://bookstore-server-sigma.vercel.app';
 
 class OpenApiInstance {
   private axios: AxiosInstance;
