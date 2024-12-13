7c299deaafbececd5b9a5d360df32ffb2399d174
sbin0819 <sbinha123@gmail.com>
2024-12-13 14:26:39 +0900

    chore: search page

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,7 +1,14 @@
+'use client';
+
+import { useSearchParams } from 'next/navigation';
+
 const SearchPage = () => {
+  const searchParams = useSearchParams();
+  const query = searchParams.get('q') || '';
+
   return (
     <div>
-      <h1>Search Page</h1>
+      <h1>{query}</h1>
     </div>
   );
 };
